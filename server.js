97c3650aada1671c04e94b4f0876234f392a8acd
kevinbloomquist <rkevinbloom@gmail.com>
2016-12-12 10:45:25 -0700

    built file skeleton and set up server.js and package.json

---
+++ server.js
@@ -0,0 +1,45 @@
+// Requirements:
+// models
+var db = require("./models");
+// Express
+var express = require('express');
+// Generate a new express app called app
+var app = express();
+// body-parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+// Serve static files from /public
+app.use(expres.static(_dirname + '/public'));
+
+/*********
+* ROUTES *
+*********/
+
+// Endpoints:
+
+// HTML Endpoints:
+
+// JSON API Endpoints:
+
+
+app.get('/api', function api_index (req, res){
+  res.json({
+    message: "Welcome to LOOPR!",
+    documentation_url: "https://github.com/kevinbloomquist/LOOPR",
+    base_url: "HEROKU LINK HERE!!!!",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes available endpoints"}
+    ]
+  });
+});
+
+
+
+/*********
+* SERVER *
+*********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
