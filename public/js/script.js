b2fce4ac7e898e8c9095ba5d8e3fa13f7eb2fbeb
kevinbloomquist <rkevinbloom@gmail.com>
2016-12-15 10:10:01 -0700

    used tunely to write some html for loop render function in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -24,3 +24,89 @@ $(document).ready(function(){
 	console.log(loopIn);
 	});
 });
+// start render JSON loops here (convert to LOOPR)
+$(document).ready(function() {
+  console.log('app.js loaded!');
+  $.get('http://localhost:3000/',function(res,req){
+
+var loopsList = res;
+console.log(loopsList);
+
+// for (i=0;i<sampleAlbums.length;i++){
+  res.forEach(function(loop) {
+    renderLoop(loop);
+
+
+  });
+});
+
+//a way to see a new loop when created ()
+// $('.createPAge form').submit(function(event){
+//   event.preventDefault();
+//   var newLoop = $(this).serialize();
+//   // working here soo close (inspect!!!)
+//   $.post('/',newLoop, function(res){
+//     console.log(res);
+  
+
+//     renderLoop(res);
+//     console.log(albumIn);
+//   });
+
+// });
+
+
+
+
+});
+function renderLoop(loop) {
+
+  console.log('rendering loop:', loop);
+
+
+  var loopHtml =
+  "        <!-- one album -->" +
+  "        <div class='row album' data-album-id='" + loop._id + "'>" +
+  "          <div class='col-md-10 col-md-offset-1'>" +
+  "            <div class='panel panel-default'>" +
+  "              <div class='panel-body'>" +
+  "              <!-- begin album internal row -->" +
+  "                <div class='row'>" +
+  "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
+  "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
+  "                  </div>" +
+  "                  <div class='col-md-9 col-xs-12'>" +
+  "                    <ul class='list-group'>" +
+  "                      <li class='list-group-item'>" +
+  "                        <h4 class='inline-header'>Album Name:</h4>" +
+  "                        <span class='album-name'>" + loop.date + "</span>" +
+  "                      </li>" +
+  "                      <li class='list-group-item'>" +
+  "                        <h4 class='inline-header'>Artist Name:</h4>" +
+  "                        <span class='artist-name'>" +  loop.message+ "</span>" +
+  "                      </li>" +
+  "                      <li class='list-group-item'>" +
+  "                        <h4 class='inline-header'>Released date:</h4>" +
+  "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
+  "                      </li>" +
+  "                    </ul>" +
+  "                  </div>" +
+  "                </div>" +
+  "                <!-- end of album internal row -->" +
+
+  "              </div>" + // end of panel-body
+
+  "              <div class='panel-footer'>" +
+  "              </div>" +
+
+  "            </div>" +
+  "          </div>" +
+  "          <!-- end one album -->";
+
+  // render to the page with jQuery
+$('.loops').append(loopHtml);
+}
+
+
+
+
